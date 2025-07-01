83970fb0e2694442dfecce5e27e27b4ecc8439f7
fared08 <fareedhq101@gmail.com>
2025-07-01 21:21:03 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:manfa_apps/main.dart';
+
+
+
+class manfaApps extends StatelessWidget {
+  const manfaApps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
